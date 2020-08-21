5cc518da20ba63815f9430f23d651aee844fcff1
diivya <divya.yadav3@wipro.com>
2020-08-21 17:03:49 +0530

    Initial Commit.

---
+++ src/test/java/divya/springframework/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package divya.springframework.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
